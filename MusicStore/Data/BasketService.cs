80f1a5b06c043b37d01deb7bf68715523ffdf235
DeathsCookie <113200622+DeathsCookie@users.noreply.github.com>
2023-01-25 23:57:08 +0000

    update

---
+++ MusicStore/Data/BasketService.cs
@@ -0,0 +1,65 @@
+﻿using MusicStore.Schemas;
+
+namespace MusicStore.Data;
+
+public class BasketService
+{
+    public readonly Dictionary<ProductSchema, int> Products = new();
+
+    public void AddProduct(ProductSchema item)
+    {
+        if (Products.ContainsKey(item))
+        {
+            Products.Add(item, 1);
+            return;
+        }
+
+        var count = Products[item];
+        count++;
+        Products.Remove(item);
+        Products.Add(item, count);
+    }
+
+    public void RemoveProduct(ProductSchema item)
+    {
+        if (!Products.ContainsKey(item))
+        {
+            return;
+        }
+
+        Products.Remove(item);
+    }
+
+    public void DecreaseProduct(ProductSchema item)
+    {
+        if (!Products.ContainsKey(item))
+        {
+            return;
+        }
+        
+        var count = Products[item];
+
+        if (count == 1)
+        {
+            RemoveProduct(item);
+            return;
+        }
+        
+        count--;
+        Products.Remove(item);
+        Products.Add(item, count);
+    }
+
+    public Double GetTotal()
+    {
+        var total = 0.0;
+        foreach (var keyValuePair in Products)
+        {
+            var subcost = keyValuePair.Key.Price * keyValuePair.Value;
+
+            total = +subcost;
+        }
+
+        return total;
+    }
+}
