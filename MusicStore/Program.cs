0cd477a8b4a6771aab0bee8f860ec046c122bdfb
DeathsCookie <113200622+DeathsCookie@users.noreply.github.com>
2023-01-25 23:57:32 +0000

    Update Program.cs

--- MusicStore/Program.cs
+++ MusicStore/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<UserService>();
+builder.Services.AddSingleton<BasketService>();
 
 var app = builder.Build();
 
