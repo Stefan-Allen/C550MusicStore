8b7ddc00124d2266e392fd0ccbc8b5ade028d9aa
DeathsCookie <113200622+DeathsCookie@users.noreply.github.com>
2023-01-26 20:22:07 +0000

    Add/Removing Products Added

--- MusicStore/Data/ProductService.cs
+++ MusicStore/Data/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using MusicStore.Schemas;
+﻿using System.Net;
+using MusicStore.Schemas;
 
 namespace MusicStore.Data;
 
@@ -11,6 +12,13 @@ public class ProductService
         db.Database.EnsureCreated();
 
         return db.Products.ToArray();
+    }
+
+    public List<ArtistSchema> GetArtists()
+    {
+        var db = new Database();
+
+        return db.Artists.ToList();
     }
 
     public ProductSchema? FindProduct(string query)
@@ -32,5 +40,47 @@ public class ProductService
         var results = db.Products.Where(p => p.Name.ToLower().Trim().Contains(query.ToLower().Trim())).ToArray();
 
         return results != Array.Empty<ProductSchema>() ? results : db.Products.Where(p => p.Artist.Name.ToLower().Trim() == query.ToLower().Trim()).ToArray();
+    }
+
+    public HttpStatusCode AddProduct(ArtistSchema artist, string name, string genre, string imageName, double price)
+    {
+        var db = new Database();
+
+        db.Database.EnsureCreated();
+
+        if (db.Products.Any(
+                p => p.Name.ToLower().Trim() == name.ToLower().Trim())) return HttpStatusCode.Conflict;
+
+        db.Products.Add(new ProductSchema
+        {
+            Id = db.Products.Max(x => x.Id) + 1,
+            Artist = artist,
+            Name = name,
+            Genre = genre,
+            ImageName = imageName,
+            Price = price
+        });
+
+        db.SaveChanges();
+
+        return HttpStatusCode.Created;
+    }
+
+    public HttpStatusCode RemoveProduct(string name)
+    {
+        var db = new Database();
+
+        db.Database.EnsureCreated();
+
+        if (!db.Products.Any(
+                p => p.Name.ToLower().Trim() == name.ToLower().Trim())) return HttpStatusCode.NotFound;
+
+        var product = db.Products.First(p => p.Name.ToLower().Trim() == name.ToLower().Trim());
+
+        db.Products.Remove(product);
+
+        db.SaveChanges();
+
+        return HttpStatusCode.Gone;
     }
 }
